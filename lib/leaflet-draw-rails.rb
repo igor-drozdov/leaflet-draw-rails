ccaf296b14e9102139cf0c7102222d49d70c8dde
iharthrush <iharthrush@gmail.com>
2015-04-06 20:07:16 +0300

    Configure initializer to precompile images.

--- lib/leaflet-draw-rails.rb
+++ lib/leaflet-draw-rails.rb
@@ -4,6 +4,9 @@ module Leaflet
   module Draw
     module Rails
       class Engine < ::Rails::Engine
+        initializer 'leaflet-draw-rails.assets.precompile' do |app|
+          app.config.assets.precompile += %w(images/spritesheet.png images/spritesheet-2x.png)
+        end
       end
     end
   end
