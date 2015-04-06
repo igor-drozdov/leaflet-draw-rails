4f1831a2eec646b33e3a24fce2b2634172d835bf
iharthrush <iharthrush@gmail.com>
2015-04-07 00:33:09 +0300

    Map: Change the default view.

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //
 
 window.addEventListener('load', function() {
-  var map = L.map('map', { drawControl: true }).setView([51.505, -0.09], 13);
+  var map = L.map('map', { drawControl: true }).setView([30, 30], 13);
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
